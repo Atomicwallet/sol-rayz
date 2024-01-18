ff4671e6f7d3a54160210761b66c9f823b950895
Aleksandr Borkov <sbor767@yandex.ru>
2024-01-18 11:19:08 +0300

    [AWC-1602] feature: Remove dist
    package.json

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { getParsedNftAccountsByOwner } from "./getParsedNftAccountsByOwner";
-export { getParsedTokenAccountsByOwner } from "./getParsedTokenAccountsByOwner";
-export { resolveToWalletAddress } from "./resolveToWalletAddress";
-export { isValidSolanaAddress, createConnectionConfig, getSolanaMetadataAddress, decodeTokenMetadata, } from "./utils";
-export declare type Options = import("./getParsedTokenAccountsByOwner").Options;
-//# sourceMappingURL=index.d.ts.map
